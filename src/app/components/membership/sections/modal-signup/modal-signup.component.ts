9482ce3cb8146c681dd5bb2b8b820326ec8c029e
shelzebub <makaxsole@gmail.com>
2018-04-11 06:07:40 -0700

    Set-up structure for Front-End Development

---
+++ src/app/components/membership/sections/modal-signup/modal-signup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-signup',
+  templateUrl: './modal-signup.component.html',
+  styleUrls: ['./modal-signup.component.css']
+})
+export class ModalSignupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/membership/sections/rulesandregulations/rulesandregulations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rulesandregulations',
+  templateUrl: './rulesandregulations.component.html',
+  styleUrls: ['./rulesandregulations.component.css']
+})
+export class RulesandregulationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
