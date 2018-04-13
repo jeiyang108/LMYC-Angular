7e00db9cf9eae8be6e0c476ea8df35fc2d1f5a4a
Meghan Tius <meghantius@gmail.com>
2018-04-12 18:36:35 -0700

    initial model wiring

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -13,11 +13,15 @@ export interface  User {
     homePhone: string;
     mobilePhone: string;
     workPhone: string;
+    membershipType: string;
+    skipperStatus: string;
     sailingQualification: string;
     skills: string;
     sailingExperience: string;
     emContactName1: string;
+    emRelationship1: string;
     emContactPhone1: string;
     emContactName2: string;
+    emRelationship2: string;
     emContactPhone2: string;
 }
